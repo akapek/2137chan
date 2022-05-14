0dc7ace55c3780b5205a82006a10688dec536418
akapek <piotrek22010@gmail.com>
2022-05-14 18:10:16 +0200

    sram papierza

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import os
 
 app = Flask(__name__)
 
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
-                               mimetype='image/vnd.microsoft.icon')
+
 
 
 if __name__ == "__main__":
